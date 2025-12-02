5cb6c6a8fd1cb1662cbf15d21443800333216678
Kristine <kristinemayg@gmail.com>
2025-12-01 23:35:38 -0500

    new store

--- index.js
+++ index.js
@@ -64,9 +64,11 @@ findProductByName(name) {
 }
 
 //Testing the system
-
+//Create products
 const ramen = new productProperties("Ramen", 2.50, 20);
 const rice = new productProperties("Rice", 16.99, 1);
 const seasoning = new productProperties("Seasoning", 3.75, 2);
 const basil = new perishableProductProperties("Basil", 1.99, 2, "2025-12-5");
 const cheese = new perishableProductProperties("Cheese", 4, 2, "2026-01-21");
+//Create store
+const newStore = new store();
