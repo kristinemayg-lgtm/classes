5eb45622c78644dedcea01ea33df01cc0e9d6777
Kristine <kristinemayg@gmail.com>
2025-12-01 22:41:36 -0500

    comments

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ class productProperties {
         this.price = price;
         this.quantity = quantity;
     }
+    //Displaying info for product properties
     displayInfo() {
         console.log(`Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`);
     }
-
+//getTotalValue: Returns the value of the product in stock
 getTotalValue() {
     return this.price * this.quantity;
 }
-
+//toString: Returns a string representation of the product
 toString() {
     return `Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`;
 }
